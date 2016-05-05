8a197080fe631080618897868e9c27a7fb36ee28
Jay Miller <jayluptonmiller@gmail.com>
2016-05-04 21:33:02 -0400

    fixed image stuff

--- code/image_scraping_jay.py
+++ code/image_scraping_jay.py
@@ -7,7 +7,7 @@ import numpy as np
 import urllib2
 from skimage import io
 
-def bin_to_nparray(cx, cy, cz=55, res=1):
+def bin_to_nparray(cx, cy, cz=55, res=1, overlay=False):
     """
     Here's the updated function.
     params:
@@ -31,12 +31,31 @@ def bin_to_nparray(cx, cy, cz=55, res=1):
     # crops = crops.T
     tiles = tiles + [[0, 1],[0, 1]]
     xr, yr = [np.arange(*tiles[i, :]) for i in [0, 1]]
-    imgs = [[tile_xy_to_nparray(x, y, res, z) for y in yr]
-             for x in xr]
-    imgs = np.array(imgs)
-    imgs = imgs.reshape((len(xr)*512, len(yr)*512))
-    return imgs[crops[0,0]:-(512-crops[0,1]),
-                crops[1,0]:-(512-crops[1,1])]
+    imgs = [[tile_xy_to_nparray(x, y, res, z, overlay) for x in xr]
+             for y in yr]
+    np_imgs = []
+    for y, imgarray in enumerate(imgs):
+        for x, img in enumerate(imgarray):
+            img = np.array(img)
+            if x == 0:
+                img = img[:,crops[0,0]:]
+            if y == 0:
+                img = img[crops[1,0]:, :]
+            if x == len(xr)-1:
+                img = img[:, :-(512-crops[0, 1])]
+            if y == len(yr)-1:
+                img = img[:-(512-crops[1, 1]), :]
+            if x == 0:
+                np_imgs.append(img)
+            else:
+                np_imgs[-1] = np.hstack((np_imgs[-1], img))
+
+    return np.vstack(tuple(np_imgs))
+
+def bin_and_overlay_to_nparray(cx, cy, cz=55, res=1):
+    img = bin_to_nparray(cx, cy, cz, res)
+    img_overlay = bin_to_nparray(cx, cy, cz, res, overlay=True)
+    return img_overlay[:,:,-1]+img*.5
 
 # make coordinates to bin boundaries mapping
 def cdata_to_pixels(cx, cy, res, binwidthx=0, binwidthy=0):
@@ -45,10 +64,12 @@ def cdata_to_pixels(cx, cy, res, binwidthx=0, binwidthy=0):
     power = 5-res
     return bounds*(2**power)
 
-def tile_xy_to_nparray(x, y, res, z=55):
+def tile_xy_to_nparray(x, y, res, z=55, overlay=False):
     end = '/'+reduce(lambda x, y: str(x) +'_'+str(y), [y, x, res])
     end += '.png'
     imgurl = 'http://openconnecto.me/ocp/catmaid/bock11/image/xy/'+str(z)+end
+    if overlay:
+        imgurl = 'http://openconnecto.me/ocp/catmaid/mp4merged/annotation/xy/'+str(z)+end
     image = io.imread(imgurl)
     return image
 
@@ -182,6 +203,6 @@ def get_bin_image(cx, cy, xwidth=0, ywidth=0, cz=55, res=1, imsize=(512,512), di
     return html_raw, canvasID
 
 if __name__ == "__main__":
-    b = bin_to_nparray(2000, 2000)
-    print b
-    print np.max(b), np.min(b)
+    b = bin_to_nparray(89, 2000, overlay=True)
+    print b.shape
+    #print np.max(b), np.min(b)
